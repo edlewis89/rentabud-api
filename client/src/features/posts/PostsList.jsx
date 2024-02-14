b6f75cc402f35ea45f73b6cd4b3f9ef9dcd21253
lewiedw <lewiedw@gmail.com>
2024-02-13 19:30:15 -0500

    Add link to titles

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { API_URL } from "../../constants";
-
+import { Link } from "react-router-dom";
 
 function PostsList(){
     const [posts, setPosts] = useState([]);
@@ -32,7 +32,9 @@ function PostsList(){
             {posts.map((post) => (
                 <div key={post.id} className="post-container">
                     <h2>
-                        {post.title}
+                        <Link to={`/posts/${post.id}`} className={"post-title"}>
+                            {post.title}
+                        </Link>
                     </h2>
                     <p>
                         {post.body}
